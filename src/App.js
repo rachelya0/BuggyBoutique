785f67fd95e91fb74a904b398fd5db789aad3aa7
Rachel Yao <42477010+rachelya0@users.noreply.github.com>
2023-01-10 01:13:38 -0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-function App() {
+function App() { 
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Router>
+        <Switch>
+
+          <Route path="/checkout" component={Checkout}>
+            <Header/>
+            <Checkout/>
+          </Route>
+
+          <Route path="/" component={Home}>
+            <Header/>
+            <Home/>
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,16 +3,19 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
 import SearchIcon from '@mui/icons-material/Search';
 import { Search } from '@mui/icons-material';
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
     return (
         <div className="header">
             {/* amazon logo */}
-            <div className="header_logo">
-                <EmojiNatureIcon className="header_logo_image" fontSize="large"></EmojiNatureIcon>
-                <h2 className="header_logo_title">Buggy Boutique</h2>
-            </div>
+            <Link to="/" style={{ textDecoration: "none" }}>
+                <div className="header_logo">
+                    <EmojiNatureIcon className="header_logo_image" fontSize="large"></EmojiNatureIcon>
+                    <h2 className="header_logo_title">Buggy Boutique</h2>
+                </div>
+            </Link>
             {/* search bar */}
             <div className="header_search">
                 <input type="text" className="header_search_input" />
@@ -28,10 +31,12 @@ function Header() {
                     <span className="nav_item_one">Returns</span>
                     <span className="nav_item_two">& Orders</span>
                 </div>
-                <div className="nav_item">
-                    <span className="nav_item_two cart_count">0</span>
-                    <AddShoppingCartIcon className="cart"></AddShoppingCartIcon>
-                </div>
+                <Link to="/checkout" style={{ textDecoration: "none" }}>
+                    <div className="nav_item">
+                        <span className="nav_item_two cart_count">0</span>
+                        <AddShoppingCartIcon className="cart"></AddShoppingCartIcon>
+                    </div>
+                </Link>
             </div>
         </div>
     )
